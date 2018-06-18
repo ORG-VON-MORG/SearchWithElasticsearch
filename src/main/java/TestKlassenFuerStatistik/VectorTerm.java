c692beb3bd3ea2e4a7ca504ee6dc4d13f584d42d
TG <test>
2018-06-18 06:17:17 +0200

    Util und VectorTerm Klasse hinzugefügt

---
+++ src/main/java/TestKlassenFuerStatistik/VectorTerm.java
@@ -0,0 +1,19 @@
+package TestKlassenFuerStatistik;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.termvectors.TermVectorsRequest;
+import org.elasticsearch.client.RestClient;
+
+public class VectorTerm {
+
+    public static void main(String[] args){
+        RestClient.builder(new HttpHost("localhost", 9200, "http"));
+
+        TermVectorsRequest termVectorsRequest = new TermVectorsRequest("last","contents.contentString","C7BVyGMBKRrm5z8MDDI8");
+
+        termVectorsRequest.
+
+    }
+
+
+}
---
+++ src/main/java/util.java
@@ -0,0 +1,8 @@
+public class util {
+
+    public static String[] StringToArrayTokenization(String string){
+        return string.split("\\s");
+
+    }
+
+}
