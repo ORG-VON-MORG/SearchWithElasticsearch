6055874a6eff955c7c1e837e5fae4d3f9611d3e9
TG <orggottschalk@web.de>
2018-07-10 21:36:30 +0200

    -Codefix -2 XML Beispieldateien hochgeladen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -70,7 +70,7 @@ public class Main {
         System.out.println(ausgabe);
         System.out.println(ausgabe.keySet());
 
-        searchWithCoreNLP.closeClient();
+       // searchWithCoreNLP.closeClient();
     }
 
 
