b6a316fe9dd1412677fa2e4d249e2a3196af0a7f
Shenna Risqianto Wilfred Piri <shennawew@outlook.com>
2018-07-17 17:19:30 +0200

    Ergebnis enthält jetzt keine duplizierte Artikeln

--- src/main/java/SearchWithDocFreq.java
+++ src/main/java/SearchWithDocFreq.java
@@ -62,28 +62,78 @@ public class SearchWithDocFreq {
            ((BoolQueryBuilder) query).should(QueryBuilders.matchQuery("contents.contentString",entry.getKey()));
         }
 
-        searchResponse = searchClient.getSearchResultFromResponse(query);
-        SearchHits hits = searchResponse.getHits();
-        SearchHit[] searchHits = hits.getHits();
+        searchResponse                      = searchClient.getSearchResultFromResponse(query);
+        SearchHits hits                     = searchResponse.getHits();
+        SearchHit[] searchHits              = hits.getHits();
+        List<result> results                = new ArrayList<result>();
         for (SearchHit hit : searchHits) {
-            String[] stringarray = new String[2];
-            //Konvertiert float score zu einem String
-            String score = Float.toString(hit.getScore());
-
-
-            String sourceAsString = hit.getSourceAsString();
             Map<String, Object> sourceAsMap = hit.getSourceAsMap();
-            stringarray[0] = (String) sourceAsMap.get("id");
-            stringarray[1] = score;
-
-
-            arrayList.add(stringarray);
+            String id                       = (String)sourceAsMap.get("id");
+            Float score                     = hit.getScore();
+            String title                    = (String)sourceAsMap.get("title");
+            String author                   = (String)sourceAsMap.get("author");
+            long pub_date                   = (Long)sourceAsMap.get("published_date");
+            //erstelle für jede Ergebnis ein Objekt
+            result att                      = new result(id, score, title, author, pub_date);
+            if (results.isEmpty()) {
+                results.add(att);
+            } else {
+                //list enthält Artikel mit gleichen title, author, und published_date?
+                if(results.contains(att)){
+                    int index               = results.indexOf(att);
+                    if(results.get(index).getScore() < att.getScore()) {
+                        //ersetzen der alte Eintrag mit der neue (höheren score)
+                        results.set(index, att);
+                    }
+                } else {
+                    results.add(att);
+                }
+            }
         }
-
+        for (result r : results) {
+            arrayList.add(new String[]{r.getId(), r.getScore().toString()});
+            //System.out.println(r.getTitle());
+        }
         return arrayList;
-
     }
 
+    /**
+     * Hilfsklasse, speichert Suchergebnisanfrage als einem Objekt
+     */
+    class result
+    {
+        private String id;
+        private Float score;
+        private String title;
+        private String author;
+        private long published_date;
 
+        result(String id, Float score, String title, String author, long published_date) {
+            this.id = id;
+            this.score = score;
+            this.title = title;
+            this.author = author;
+            this.published_date = published_date;
+        }
+
+        String getId() { return this.id; }
+        String getTitle() { return this.title; }
+        String getAuthor() { return this.author; }
+        long getPublished_date() { return this.published_date; }
+        Float getScore() { return this.score; }
+
+        /**
+         * prüfe, ob 2 Artikel gleich sind
+         */
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof result) {
+                return this.title.equals(((result) obj).getTitle()) &&
+                       this.author.equals(((result) obj).getAuthor()) &&
+                       this.published_date == (((result) obj).getPublished_date());
+            }
+            return false;
+        }
+    }
 
 }
